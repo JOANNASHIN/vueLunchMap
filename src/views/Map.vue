2f9bdc86f6db6ccf05a620102629e4e3edc03efe
jieunshin <joannashin3@forbiz.co.kr>
2021-12-08 17:52:00 +0900

    카카오 map

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -9,28 +9,82 @@
 <script>
 import { ref, onMounted } from "vue";
 import SearchComponent from "../components/SearchComponent";
+// import axios from "axios";
 
 export default {
+    name: "Map",
+
     components: {
         SearchComponent
     },
+    
     setup() {
         const lunchMap = ref(null);
 
+        const fetchRestaurantList = () => {
+            // axios
+        }
+
         const initMap = () => {
-            const container = lunchMap.value;
-            const markerPosition = new kakao.maps.LatLng(35.19656853772262, 129.0807270648317);
-            
-            const map = new kakao.maps.Map(container, {
-                center: markerPosition,
-                level: 3
+            const defaultData = {
+                mapContainer: lunchMap.value,
+                centerPosition: new kakao.maps.LatLng(37.481167099868166, 127.038415684882),
+                markerSrc: "//t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png",
+                markerSize: new kakao.maps.Size(24, 35),
+                positions:  [
+                    {
+                        title: '모범 떡볶이', 
+                        latlng: new kakao.maps.LatLng(37.48512597601747, 127.03206392495402)
+                    },
+                    {
+                        title: '경성불백', 
+                        latlng: new kakao.maps.LatLng(37.479162161747034, 127.04053519812724)
+                    },
+                    {
+                        title: '춘천 우리 닭갈비', 
+                        latlng: new kakao.maps.LatLng(37.48112248605404, 127.04116125789652)
+                    },
+                    {
+                        title: '노브랜드 버거',
+                        latlng: new kakao.maps.LatLng(37.48534890822267, 127.03440779812728)
+                    }
+                ]
+            }
+
+            const { mapContainer, centerPosition, markerSrc, markerSize, positions} = defaultData;
+              
+            const markerImage = new kakao.maps.MarkerImage(markerSrc, markerSize); 
+            const map = new kakao.maps.Map(mapContainer, {
+                center: centerPosition,
+                level: 4
             })
 
-            const marker = new kakao.maps.Marker({
-                position: markerPosition
-            });
+            const makeOverListener = (map, marker) => {
+                return () => {
+                    console.log("클릭", map, marker)
+                };
+            }
 
-            marker.setMap(map)
+            positions.forEach(location => {
+                const content = `<span class="fb__restaurant__title">${location.title}</span>`
+                const marker = new kakao.maps.Marker({
+                    map,
+                    position: location.latlng,
+                    title : location.title,
+                    image : markerImage,
+                });
+
+                new kakao.maps.CustomOverlay({
+                    map,
+                    content,
+                    position: location.latlng,
+                    xAnchor: 0.5,
+                    yAnchor: 0
+                });
+
+                kakao.maps.event.addListener(marker, 'click', makeOverListener(map, marker));
+                marker.setMap(map)
+            })
         }
 
         onMounted(() => {
@@ -40,15 +94,16 @@ export default {
             else {
                 const script = document.createElement('script')
                 script.onload = () => kakao.maps.load(initMap);
-                script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=b5d70053a01fa96cdc3ff5bf1b2dca51'
+                script.src = '//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=b5d70053a01fa96cdc3ff5bf1b2dca51'
                 document.head.appendChild(script)
             }
         })
 
+        fetchRestaurantList();
+
         return {
-            lunchMap
+            lunchMap,
         }
-        
     }
 }
 </script>
