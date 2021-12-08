1428323d98a33c1142a2b3ca4870d8db90abc042
jieunshin <joannashin3@forbiz.co.kr>
2021-12-08 10:21:13 +0900

    rem설정

--- src/main.js
+++ src/main.js
@@ -3,4 +3,35 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+
 createApp(App).use(store).use(router).mount("#app");
+
+
+/* rem */
+const htmlDoc = document.documentElement;
+const bodyDoc = document.querySelector("body");
+let enSizing = false;
+
+function setFontSize() {
+    if (window.innerWidth > window.innerHeight || window.innerWidth > 1100) {
+        htmlDoc.style.fontSize = "";
+        bodyDoc.setAttribute("type", "pc");
+
+       return ;
+    }
+    htmlDoc.style.fontSize = parseInt(htmlDoc.offsetWidth / 360 * 100) + '%';
+
+    bodyDoc.setAttribute("type", "mobile");
+}
+ 
+window.onresize = function() {
+    if (!enSizing) {
+        window.requestAnimationFrame(function() {
+            setFontSize();
+            enSizing = false;
+        });
+    }
+    enSizing = true;
+}
+ 
+window.dispatchEvent(new Event('resize'));
