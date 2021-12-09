18c1473eb27af3f9d4765ef7d275423be2dd9cd4
jieunshin <joannashin3@forbiz.co.kr>
2021-12-09 18:00:29 +0900

    리스트 컴포넌트 생성

---
+++ src/components/RestaurantList.vue
@@ -0,0 +1,30 @@
+<template>
+    <article class="fb__list">
+        <h2>레스토랑 리스트</h2>
+        <div>
+            <!-- {{selectedMenu.name}} -->
+        </div>
+    </article>
+</template>
+
+<script>
+import { useStore } from "vuex";
+
+export default {
+    name: "RestaurantList",
+    props: {
+        selectedMenu: {
+            type: Object,
+            required: false,
+        }
+    },
+    setup() {
+        const store = useStore();
+        // store.state.
+    }
+}
+</script>
+
+<style>
+
+</style>
